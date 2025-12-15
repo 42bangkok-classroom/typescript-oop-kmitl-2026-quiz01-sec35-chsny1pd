323566a85c9df73f2a648c115ffc510d879caf9a
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2025-12-15 10:06:30 +0700

    3

--- src/q02/atm.ts
+++ src/q02/atm.ts
@@ -1,11 +1,11 @@
 const balance = +process.argv[2]
 const amount = +process.argv[3]
 
-if (Number.isNaN(balance)||Number.isNaN(amount)||balance<0||amount<0)
+if (Number.isNaN(balance)||balance<0||amount<0)
 {
   console.log('Invalid Input')
 }
-else if(amount>balance)
+else if(amount>=balance)
 {
   console.log('Insufficient balance')
 }
