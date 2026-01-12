9f827ee35ac329892be4c85d7d44c66ae878cfa2
Setsin Chutsanayothin <68011112@kmitl.ac.th>
2026-01-12 18:16:57 +0700

    13

--- src/q06/xo.ts
+++ src/q06/xo.ts
@@ -1,19 +1,20 @@
-const input = +process.argv[2]
+const input = process.argv[2]
+const input2 = parseInt(input)
 
-if(Number.isNaN(input) || input<=0)
+if(Number.isNaN(input) || input2<=0)
 {
   console.log('Invalid Input')
 }
-for(let i = 1;i<=input;i++)
+for(let i = 1;i<=input2;i++)
 {
   let result = ''
-  for(let j = 1;j<=input;j++)
+  for(let j = 1;j<=input2;j++)
   {
-    if(i==1 ||i==input){
+    if(i==1 ||i==input2){
     result += 'x'
     }
     else {
-      if(j==1 ||j==input){
+      if(j==1 ||j==input2){
       result += 'x'
       }
       else {
