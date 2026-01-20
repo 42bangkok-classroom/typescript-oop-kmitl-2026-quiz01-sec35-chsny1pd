45a5514fb836ae003049625354e2092a95625803
PD <168636679+chsny1pd@users.noreply.github.com>
2026-01-21 01:16:09 +0700

    5

--- src/q05/foobar.ts
+++ src/q05/foobar.ts
@@ -1,27 +1,53 @@
-const num = +process.argv[2]
-if (Number.isNaN(num)||num<1||!Number.isInteger(num))
-{
+// const num = +process.argv[2]
+// if (Number.isNaN(num)||num<1||!Number.isInteger(num))
+// {
+//   console.log('Invalid Input')
+// }
+// else
+// {
+//   for (let i = 1; i<num+1;i++)
+// {
+//   if(i%3 ==0 && i%7 ==0)
+//   {
+//     console.log('FooBar')
+//   }
+//   else if(i%3 ==0 )
+//   {
+//     console.log('Foo')
+//   }
+//   else if(i%7 ==0)
+//   {
+//     console.log('Bar')
+//   }
+//   else
+//   {
+//     console.log(String(i))
+//   }
+// }
+// }
+const input = process.argv[2]
+if(input === undefined||input.trim()==''){
   console.log('Invalid Input')
 }
-else
-{
-  for (let i = 1; i<num+1;i++)
-{
-  if(i%3 ==0 && i%7 ==0)
-  {
-    console.log('FooBar')
+else{
+  const num = +input
+  if(isNaN(num)||num>0||!Number.isInteger(num)){
+    console.log('Invalid Input')
   }
-  else if(i%3 ==0 )
-  {
-    console.log('Foo')
+  else{
+    for(let i = 0 ; i <= num ; i++){
+      if(num%7==0 &&num%3==0){
+      console.log('FooBar')
+      }
+      else if(num%3==0){
+      console.log('Foo')
+      }
+      else if(num%7==0 ){
+      console.log('Bar')
+      }
+      else{
+      console.log(String(i))
+      }
+    }
   }
-  else if(i%7 ==0)
-  {
-    console.log('Bar')
-  }
-  else
-  {
-    console.log(String(i))
-  }
-}
 }
--- src/q06/xo.ts
+++ src/q06/xo.ts
@@ -1,28 +1,58 @@
-const input = +process.argv[2]
+// const input = +process.argv[2]
 
-if(Number.isNaN(input) || input<=0||!Number.isInteger(input))
-{
+// if(Number.isNaN(input) || input<=0||!Number.isInteger(input))
+// {
+//   console.log('Invalid Input')
+// }
+// else
+// {
+//   for(let i = 1;i<=input;i++)
+// {
+//   let result = ''
+//   for(let j = 1;j<=input;j++)
+//   {
+//     if(i==1 ||i==input){
+//     result += 'x'
+//     }
+//     else {
+//       if(j==1 ||j==input){
+//       result += 'x'
+//       }
+//       else {
+//       result += 'o'
+//       }
+//     }
+//   }
+//   console.log(result)
+// }
+// }
+
+const input = process.argv[2]
+if(input === undefined || input.trim()==''){
   console.log('Invalid Input')
 }
-else
-{
-  for(let i = 1;i<=input;i++)
-{
-  let result = ''
-  for(let j = 1;j<=input;j++)
-  {
-    if(i==1 ||i==input){
-    result += 'x'
-    }
-    else {
-      if(j==1 ||j==input){
-      result += 'x'
+else{
+  const num = +input
+  if(isNaN(num)||num<=0||!Number.isInteger(num)){
+    console.log('Invalid Input')
+  }
+  else{
+    for(let i = 0 ; i < num ; i++){
+      let result = ''
+      for(let j = 0 ; j < num ; j++){
+        if(i==0||i==num-1){
+          result += 'x'
+        }
+        else{
+          if(j==0||j==num-1){
+            result += 'x'
+          }
+          else{
+            result += 'o'
+          }
+        }
       }
-      else {
-      result += 'o'
-      }
+      console.log(result)
     }
   }
-  console.log(result)
-}
 }
