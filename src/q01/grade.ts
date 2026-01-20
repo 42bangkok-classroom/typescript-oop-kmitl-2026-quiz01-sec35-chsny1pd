5fd69c92f13f8f293a7b74ec9a3d0e0f49f995ed
PD <168636679+chsny1pd@users.noreply.github.com>
2026-01-21 00:22:22 +0700

    t1

--- src/q01/grade.ts
+++ src/q01/grade.ts
@@ -1,33 +1,57 @@
+// const input = process.argv[2]
+// if (input === undefined || input.trim() === '')
+// {
+//  console.log('Invalid Input')
+// }
+// else 
+// {
+//   const score = +input
+//   if(Number.isNaN(score)||score<0||score>100)
+//   {
+//     console.log('Invalid Input')
+//   }
+// else if(score>=80)
+// {
+//   console.log('Grade is A')
+// }
+// else if(score>=70)
+// {
+//   console.log('Grade is B')
+// }
+// else if(score>=60)
+// {
+//   console.log('Grade is C')
+// }
+// else if(score>=50)
+// {
+//   console.log('Grade is D')
+// }
+// else 
+// {
+//   console.log('Grade is F')
+// }
+// }
 const input = process.argv[2]
-if (input === undefined || input.trim() === '')
-{
- console.log('Invalid Input')
+if (input === undefined || input.trim() == ""){
+  console.log('Invalid Input')
 }
-else 
-{
-  const score = +input
-  if(Number.isNaN(score)||score<0||score>100)
-  {
-    console.log('Invalid Input')
-  }
-else if(score>=80)
-{
+
+const score = +input
+if (isNaN(score)||score<0||score>100){
+  console.log('Invalid Input')
+}
+else if(score>=80){
   console.log('Grade is A')
 }
-else if(score>=70)
-{
+else if(score>=70){
   console.log('Grade is B')
 }
-else if(score>=60)
-{
+else if(score>=60){
   console.log('Grade is C')
 }
-else if(score>=50)
-{
+else if(score>=50){
   console.log('Grade is D')
 }
-else 
-{
+else{
   console.log('Grade is F')
-}
 }
--- src/q02/atm.ts
+++ src/q02/atm.ts
@@ -1,27 +1,47 @@
+// const input1 = process.argv[2]
+// const input2 = process.argv[3]
+// if (input1 === undefined || input1.trim() === '' ||input2 === undefined || input2.trim() === '')
+// {
+//  console.log('Invalid Input')
+// }
+// else
+// {
+//   const balance = +input1
+//   const amount = +input2
+//   if (Number.isNaN(balance)||Number.isNaN(amount)||balance<0||amount<0||String(balance)==''||String(amount)=='')
+// {
+//   console.log('Invalid Input')
+// }
+// else if(amount>=balance)
+// {
+//   console.log('Insufficient balance')
+// }
+// else if(amount>5000)
+// {
+//   console.log('Exceeds per-withdrawal limit')
+// }
+// else
+// {
+//   console.log('Withdrawal approved')
+// }
+// }
 const input1 = process.argv[2]
 const input2 = process.argv[3]
-if (input1 === undefined || input1.trim() === '' ||input2 === undefined || input2.trim() === '')
-{
- console.log('Invalid Input')
-}
-else
-{
-  const balance = +input1
-  const amount = +input2
-  if (Number.isNaN(balance)||Number.isNaN(amount)||balance<0||amount<0||String(balance)==''||String(amount)=='')
-{
+if (input1 === undefined || input1.trim()==''||input2 === undefined || input2.trim()==''){
   console.log('Invalid Input')
 }
-else if(amount>=balance)
-{
+const amount = +input1
+const balance = +input2
+
+if(isNaN(amount)||amount<0||isNaN(balance)||balance<0){
+  console.log('Invalid Input')
+}
+else if(amount>balance){
   console.log('Insufficient balance')
 }
-else if(amount>5000)
-{
+else if(amount>5000){
   console.log('Exceeds per-withdrawal limit')
 }
-else
-{
+else{
   console.log('Withdrawal approved')
-}
 }
